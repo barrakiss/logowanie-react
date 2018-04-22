a5e1e0d9392fb331b6c44d8652701569bcb8613d
bartekmanu <manowski.b@gmail.com>
2018-04-22 21:54:12 +0200

    tests

---
+++ src/Login.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginForm from './base/containers/LoginForm.jsx';
+
+it('should login user with correct password', () => {
+  //Arrange
+  let isLoggedIn = false;
+  const loginUser = () => {
+    isLoggedIn = true;
+  };
+  const loginForm = mount(<LoginForm loginUser={loginUser} />);
+
+  loginForm.find('#email').text('test@test.test');
+  loginForm.find('#password').text('Password1');
+  loginForm.find('#submit').simulate('click');
+
+  //Assert
+  expect(isLoggedIn).toBeTruthy();
+});
